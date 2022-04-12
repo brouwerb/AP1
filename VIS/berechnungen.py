f2338a5f03614d8b92e556561a3eef79829921ff
brouwerb <benedictbrouwer@gmail.com>
2022-04-12 09:40:10 +0200

    berechnung

--- VIS/berechnungen.py
+++ VIS/berechnungen.py
@@ -2,6 +2,7 @@ import math
 from sympy import *
 from sympy.abc import i
 #  Variablen
+x = symarray("x", 5)
 
 x = IndexedBase("x")
 uerr = IndexedBase("ui")
@@ -23,7 +24,8 @@ gausserr = sqrt(Sum((diff(nabla, x[i])**2*uerr[i]**2), (i, 0, 4)))
 
 print(latex(gausserr))
 
-nab = lambdify([x[0], s, m, g, t, rhof], nabla)
+nab = lambdify([x[0], x[1], x[2], x[3], x[4]], nabla)
+gauss = lambdify([x[0], x[1], x[2], x[3], x[4], uerr[0], uerr[1], uerr [2], uerr[3], uerr [4]], gausserr)
 
 r = 0.0039975 #0
 s = 0.35 #1
@@ -32,6 +34,11 @@ g = 9.807232
 t = 3.838235 #3
 rhof = 0.001222 #4
 
-ur = 0.0017
+ur = 0.000017
+us = 0.00021
+um = 0.0000005
+ut = 0.14425
+urho = 0.0000021
 
-print(nab(r, s, m, g, t, rhof))
+print(nab(r, s, m, t, rhof))
+print(gauss(r, s, m, t, rhof, ur, us, um, ut, urho))
