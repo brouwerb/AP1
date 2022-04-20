8914a5b02aea35fa28ad5b0f51c80c139f8b1a1a
Jascha Fricker <frickerjascha@gmail.com>
2022-04-20 18:34:23 +0200

    asdf

--- ZUS/Python/4Plot_of_Hell.py
+++ ZUS/Python/4Plot_of_Hell.py
@@ -6,36 +6,42 @@ import matplotlib as mpl
 from scipy import optimize
 import roundwitherror as re
 import string
-muly = 1e5
-mulx = 1e-6
+
+n=0.002613
+R=8.314462618
+Kelvin = 273.15
+muly = 1*0.1#1e5
+mulx = 1*1/n#1e-6
+Tempgenau =0.1
 
 X_START =0
 Y_START =12 * muly
 X_END = 4.2 * mulx
 Y_END = 45.5 *muly
 TITEL = "Ordnung der Maxima in Bezug zur Röhrenlänge"
-Y_LABEL = r"$p*V$   in   $hpa*mm^3$"
-X_LABEL = r"$\frac{1}{V}$    in    $\frac{1}{cm^3}$"
+Y_LABEL = r"Druck $p$ in $mpa$"
+X_LABEL = r"Molares Volumen $V_m$ in $\frac{cm^3}{mol}$"
 X_ERROR = 4
 Y_ERROR = 1
-X_MAJOR_TICK = 1 *mulx
+X_MAJOR_TICK = 250
 Y_MAJOR_TICK = 5 *muly
-X_MINOR_TICK =0.2 * mulx
+X_MINOR_TICK = 50
 Y_MINOR_TICK = 1 *muly
 SAVE_AS = "./ZUS/Plots/4Plot_of_Hell.pdf"
 POINT_STYLE = ["o","^","s"]
 COLOR_STYLE =["blue","red","green"]
 
-n=0.002613
-R=8.314462618
-Kelvin = 273.15
+
+
 def Alind(String):
     return [string.ascii_uppercase.index(String[0]),int(String[1:])-1]
 
-def Theo(x,a,b,T):
-    return n*R*T/(x-(n*b))-n**2*a/x
+def Theo(x,a,b,c,d,e,g):
+    return a+b*(x-g)+c*(x-g)**2+d*(x-g)**3+e*(x-g)**4
+
+
 def TheoArr(x,p):
-    return Theo(x,p[0],p[1],p[2])
+    return Theo(x,p[0],p[1],p[2],p[3],p[4],p[5])
 
 def getValFromCell(Cell,path,sheet):
     workbook = xlrd.open_workbook(path)
@@ -53,10 +59,10 @@ def getAxisFromCell(Cell1,Cell2,path,sheet,plusCol=0):
         data.append(worksheet.cell(i, collumn1).value)    
     return data
 
-def polynom(x,a,b,c,d,e,f,g):
-    return a+b*(x-g)+c*(x-g)**2+d*(x-g)**2+e*(x-g)**2+f*(x-g)**2
+def polynom(x,a,b,c,d,e,g):
+    return a+b*(x-g)+c*(x-g)**2+d*(x-g)**3+e*(x-g)**4
 def polynomArr(x,p):
-    return polynom(x,p[0],p[1],p[2],p[3],p[4],p[5],p[6])
+    return polynom(x,p[0],p[1],p[2],p[3],p[4],p[5])
 
 TempsCell = [["B1","B53","B103"],["A1","C1","E1"],["A1","E1","I1"]]
 Axis = [[[["A3","A25"],["A29","A51"]],[["A54","A76"],["A80","A101"]],[["A105","A141"],["A148","A171"]]],[[["A3","A25"],["A3","A25"]],[["C3","C25"],["C3","C25"]],[["E3","E23"],["E3","E23"]]],[[["A3","A25"],["A29","A51"]],[["E3","E25"],["E29","E51"]],[["I3","I23"],["I26","I58"]]]]
@@ -82,41 +88,47 @@ for i in range(3):
         x[i].append([getAxisFromCell(Axis[i][j][0][0],Axis[i][j][0][1],path,"Daten"),getAxisFromCell(Axis[i][j][0][0],Axis[i][j][0][1],path,"Daten")])
         y[i].append([getAxisFromCell(Axis[i][j][0][0],Axis[i][j][0][1],path,"Daten",plusCol=1),getAxisFromCell(Axis[i][j][0][0],Axis[i][j][0][1],path,"Daten",plusCol=1)])
         
-        x[i][j][0]= [x[i][j][0][k]*1e-6 for k in range(len(x[i][j][0]))]
-        y[i][j][0]= [y[i][j][0][k]*1e5 for k in range(len(y[i][j][0]))]
+        x[i][j][0]= [x[i][j][0][k]*mulx for k in range(len(x[i][j][0]))]
+        y[i][j][0]= [y[i][j][0][k]*muly for k in range(len(y[i][j][0]))]
         #print(x[i][j])
         # for k,K in enumerate(x[i][j][0]):
         #     if K in x[i][j][1]:
         #         x[i][j][0][k]=K+x[i][j][1][x[i][j][1].index(K)]/2
-        poptBuf,perBuf = optimize.curve_fit(Theo,x[i][j][0],y[i][j][0],bounds=[[3.95-0.01,0.000197-0.0000001,Temps[i][j]-0.01],[3.95+0.01,0.000197+0.0000001,Temps[i][j]+0.01]])
-        popt[i].append(poptBuf)
-        perr[i].append(np.sqrt(np.diag(perBuf)))
-        print(popt[i][j])
+        #poptBuf,perBuf = optimize.curve_fit(Theo,x[i][j][0],y[i][j][0],bounds=[[-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf],[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf]])
+        #popt[i].append(poptBuf)
+        #perr[i].append(np.sqrt(np.diag(perBuf)))
+        #print(popt[i][j])
 
 fig, ax = plt.subplots()
 ax.grid()
 for i in range(len(x)):
     for j in range(len(x[i])):
-        xy = re.genDataFromFunktion(200,X_START,X_END,popt[i][j],TheoArr)
+        #xy = re.genDataFromFunktion(200,X_START,X_END,popt[i][j],TheoArr)
         
-        ax.scatter(x[i][j][0],y[i][j][0],s=10,linewidths=1,edgecolors="black",zorder=10)
+        ax.scatter(x[i][j][0],y[i][j][0],s=10,linewidths=0.5,edgecolors="black",zorder=10)
         
-        theo,=ax.plot(xy[0],xy[1],color= COLOR_STYLE[1],linestyle="dotted",linewidth=0.7)
+        #theo,=ax.plot(xy[0],xy[1],color= COLOR_STYLE[1],linestyle="dotted",linewidth=0.7)
 ubergangs =[[],[]]
 for i in range(len(FluAb)):
     for j in range(len(FluAb[0])):
-        ubergangs[0].append(FluAb[i][j][0]*1e-6)
-        ubergangs[0].append(GaBis[i][j][0]*1e-6)
-        ubergangs[1].append(FluAb[i][j][1]*1e5)
-        ubergangs[1].append(GaBis[i][j][1]*1e5)
-        ax.scatter(FluAb[i][j][0]*1e-6,FluAb[i][j][1]*1e5,s=40,facecolors="none",edgecolors="purple",linewidths=1.5)
-        ax.scatter(GaBis[i][j][0]*1e-6,GaBis[i][j][1]*1e5,s=40,facecolors="none",edgecolors="purple",linewidths=1.5)
-polpar,polerr =optimize.curve_fit(polynom,ubergangs[0],ubergangs[1],bounds=[[-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf],[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf]])
+        ubergangs[0].append(FluAb[i][j][0]*mulx)
+        ubergangs[0].append(GaBis[i][j][0]*mulx)
+        ubergangs[1].append(FluAb[i][j][1]*muly*10)
+        ubergangs[1].append(GaBis[i][j][1]*muly*10)
+        ax.scatter(FluAb[i][j][0]*mulx,FluAb[i][j][1]*muly,s=40,facecolors="none",edgecolors="orange",linewidths=1.5)
+        ax.scatter(GaBis[i][j][0]*mulx,GaBis[i][j][1]*muly,s=40,facecolors="none",edgecolors="purple",linewidths=1.5)
+polpar,polerr =optimize.curve_fit(polynom,ubergangs[0],ubergangs[1],bounds=[[34,2e-2,-5e-4,0,-9e-10,150],[40,5e-2,-2e-4,7e-6,0,200]])
 pol = re.genDataFromFunktion(200,X_START,X_END,polpar,polynomArr)
-ax.plot(pol[0],pol[1],color= "black",linestyle="dotted",linewidth=1)
+ax.plot(pol[0],[pol[1][i]*0.1 for i in range(len(pol[1]))],color= "black",linestyle="--",linewidth=1)
 print(polpar)
 
-#ax.legend([sc,theo],[r"Messdaten",r"Fit : $ax+b$ mit"+"\n"+f"a={re.round_err(popt[0],perr[0])} , b={re.round_err(popt[1],perr[1])}"])
+
+
+Temps =[re.round_err(Temps[i][j],Tempgenau) + r" $°$"for i in range(len(Temps)) for j in range(len(Temps[i]))]
+Temps.append("Kondensationspunkt")
+Temps.append("Vollständig flüssig")
+ax.legend(Temps)
+
 ax.set(xlabel=X_LABEL, ylabel=Y_LABEL)
 #ax.scatter(x,y,marker='x',color="C0")
 #ax.plot([X_START,X_END],[reg.intercept,intercept+X_END*slope],color="red",linewidth=0.8)
